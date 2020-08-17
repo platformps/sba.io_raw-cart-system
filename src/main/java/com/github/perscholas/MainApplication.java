f79261070c2f4a7273e040e990f52e74c9159933
Leon Christopher Hunter <xleonhunter@gmail.com>
2020-08-17 13:34:21 -0400

    Delete MainApplication.java

--- src/main/java/com/github/perscholas/MainApplication.java
+++
@@ -1,8 +0,0 @@
-package com.github.perscholas;
-
-public class MainApplication {
-    public static void main(String[] args) {
-        Runnable myObject = new MyObject();
-        myObject.run();
-    }
-}
